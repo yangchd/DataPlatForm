d6fe1672e46300cc0a5d0b1adc9162b14615c9b2
yangchd <yangchd@yonyou.com>
2017-09-20 14:09:21 +0800

    创建工程

--- page/src/components/Hello.vue
+++ page/src/components/Hello.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://vuejs.org" target="_blank">aaasdsdaaaa</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
--- page/src/router/index.js
+++ page/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
